c23201b346b8f72cc949c204144f513dc0dcdc28
Nicolai Wolf <119791859+Nico1395@users.noreply.github.com>
2024-08-15 20:57:12 +0200

    DbContext and entity configuration registration #8

---
+++ src/ChronoFlow.Server.Common/Persistence/Context/ChronoFlowDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ChronoFlow.Server.Common.Persistence.Context;
+
+internal sealed class ChronoFlowDbContext(
+    DbContextModelOptions<ChronoFlowDbContext> modelOptions,
+    DbContextOptions<ChronoFlowDbContext> options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        foreach (var assembly in modelOptions.GetEntityConfigurationAssemblies())
+            modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+    }
+}
---
+++ src/ChronoFlow.Server.Common/Persistence/Context/DbContextModelOptionsExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ChronoFlow.Server.Common.Persistence.Context;
+
+public static class DbContextModelOptionsExtensions
+{
+    public static IServiceCollection AddDbContextModelOptions<TDbContext>(this IServiceCollection services, Action<DbContextModelOptions<TDbContext>> optionsAction)
+        where TDbContext : DbContext
+    {
+        var modelOptions = new DbContextModelOptions<TDbContext>();
+        optionsAction.Invoke(modelOptions);
+
+        return services.AddSingleton(sp => modelOptions);
+    }
+}
