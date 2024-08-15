760f169e5912585cad215d1fd8ea9fa28f02ae58
Nicolai Wolf <119791859+Nico1395@users.noreply.github.com>
2024-08-15 20:24:07 +0200

    Created common and access management server projects #8

---
+++ src/ChronoFlow.Server/Program.cs
@@ -0,0 +1,23 @@
+namespace ChronoFlow.Server;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.Run();
+    }
+}
