d9cfe7eea3d06ccccf17eb904be561a592f859ee
N.Wolf <119791859+Nico1395@users.noreply.github.com>
2024-08-16 11:01:19 +0200

    Role data access #11

---
+++ src/ChronoFlow.Server.AccessManagement/Roles/Persistence/RoleReadRepository.cs
@@ -0,0 +1,8 @@
+﻿using ChronoFlow.Server.AccessManagement.Roles.Entities.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChronoFlow.Server.AccessManagement.Roles.Persistence;
+
+internal sealed class RoleReadRepository(DbContext _dbContext) : IRoleReadRepository
+{
+}
