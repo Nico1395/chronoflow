70dfa12b7b36fb61a186f590dc263bf0abc3138d
Nicolai Wolf <119791859+Nico1395@users.noreply.github.com>
2024-08-17 15:16:21 +0200

    WebAssembly client and common project #9

---
+++ src/ChronoFlow.Client.Common/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ChronoFlow.Client.Common;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddCommon(this IServiceCollection services)
+    {
+        return services;
+    }
+}
---
+++ src/ChronoFlow.Client/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using ChronoFlow.Client.Common;
+
+namespace ChronoFlow.Client;
+
+internal static class DependencyInjection
+{
+    internal static IServiceCollection AddChronoFlow(this IServiceCollection services)
+    {
+        services.AddCommon();
+
+        return services;
+    }
+}
