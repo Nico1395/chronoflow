78cc0e319e54680c407ae0de094babab17054bae
Nicolai Wolf <119791859+Nico1395@users.noreply.github.com>
2024-08-18 14:35:41 +0200

    Login page draft #9

---
+++ src/ChronoFlow.Client.Common/Authentication/UseCases/Login/RequestLoginResult.cs
@@ -0,0 +1,5 @@
+﻿namespace ChronoFlow.Client.Common.Authentication.UseCases.Login;
+
+internal sealed record RequestLoginResult(
+    RequestLoginResultCode Code,
+    string? Message = null);
--- src/ChronoFlow.Client.Common/DependencyInjection.cs
+++ src/ChronoFlow.Client.Common/DependencyInjection.cs
@@ -1,6 +1,8 @@
+using ChronoFlow.Client.Common.Authentication.UseCases.Login;
 ﻿using ChronoFlow.Client.Common.Browser;
 using ChronoFlow.Client.Common.Localization.DependencyInjection;
 using ChronoFlow.Client.Common.Localization.Resources;
+using ChronoFlow.Client.Common.Validation;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ChronoFlow.Client.Common;
@@ -9,11 +11,16 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddCommon(this IServiceCollection services)
     {
+        // Localization
         services.AddLocalizer(options =>
         {
             options.AddResource<LocalizationResources>();
         });
 
+        // Authentication
+        services.AddScoped<ILoginPageService, LoginPageService>();
+
+        // Browser
         services.AddScoped<ILocalStorage, LocalStorage>();
         services.AddScoped<ISessionStorage, SessionStorage>();
         services.AddScoped<IBrowserLogger, BrowserLogger>();
