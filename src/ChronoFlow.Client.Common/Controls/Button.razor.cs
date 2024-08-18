ead1de25f8e251907af7dc074372feedaf3d9791
Nicolai Wolf <119791859+Nico1395@users.noreply.github.com>
2024-08-18 11:42:46 +0200

    Button component #9

---
+++ src/ChronoFlow.Client.Common/Controls/Button.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ChronoFlow.Client.Common.Controls;
+
+public partial class Button : ComponentBase
+{
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    [Parameter]
+    public string? Id { get; set; }
+
+    [Parameter]
+    public string Type { get; set; } = "button";
+
+    [Parameter]
+    public string? IconLeft { get; set; }
+
+    [Parameter]
+    public string? IconLeftSize { get; set; }
+
+    [Parameter]
+    public string? Tooltip { get; set; }
+
+    [Parameter]
+    public EventCallback OnClick { get; set; }
+}
