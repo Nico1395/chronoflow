2f5607842bb295915d2edfec4c8370789356d331
Nicolai Wolf <119791859+Nico1395@users.noreply.github.com>
2024-08-23 22:19:59 +0200

    Role main data view model service #9 #18

--- src/ChronoFlow.Client.AccessManagement/Roles/Persistence/RoleMainDataViewModelService.cs
+++ src/ChronoFlow.Client.AccessManagement/Roles/Persistence/RoleMainDataViewModelService.cs
@@ -13,23 +13,23 @@ internal sealed class RoleMainDataViewModelService(IServerHttpRequestService _ht
         return _httpRequestService.GetAsync<List<RoleDto>, List<RoleViewModel>>("api/access-management/roles/get-all", cancellationToken);
     }
 
-    public Task<Result<RoleViewModel?>> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    public Task<Result<RoleViewModel>> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        return _httpRequestService.GetAsync<RoleDto, RoleViewModel>("api/access-management/roles/get-by-id", cancellationToken, ("roleId", id));
     }
 
     public Task<Result> AddAsync(RoleViewModel viewModel, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        return _httpRequestService.PostAsync<RoleDto, RoleViewModel>("api/access-management/roles/add", viewModel, cancellationToken);
     }
 
     public Task<Result> UpdateAsync(RoleViewModel viewModel, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        return _httpRequestService.PatchAsync<RoleDto, RoleViewModel>("api/access-management/roles/update", viewModel, cancellationToken);
     }
 
     public Task<Result> DeleteAsync(RoleViewModel viewModel, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        return _httpRequestService.DeleteAsync("api/access-management/roles/delete", cancellationToken, ("roleId", viewModel.Id));
     }
 }
--- src/ChronoFlow.Client.Common/MainData/Persistence/IMainDataViewModelService.cs
+++ src/ChronoFlow.Client.Common/MainData/Persistence/IMainDataViewModelService.cs
@@ -6,7 +6,7 @@ public interface IMainDataViewModelService<TViewModel>
     where TViewModel : class
 {
     public Task<Result<List<TViewModel>>> GetAllAsync(CancellationToken cancellationToken = default);
-    public Task<Result<TViewModel?>> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    public Task<Result<TViewModel>> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
     public Task<Result> AddAsync(TViewModel viewModel, CancellationToken cancellationToken = default);
     public Task<Result> UpdateAsync(TViewModel viewModel, CancellationToken cancellationToken = default);
     public Task<Result> DeleteAsync(TViewModel viewModel, CancellationToken cancellationToken = default);
