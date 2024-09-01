6f5657d9fcf6efaf29256ebb6aa483b16bcd5321
Nicolai Wolf <119791859+Nico1395@users.noreply.github.com>
2024-09-01 16:44:31 +0200

    Role main data list item details #18

--- src/ChronoFlow.Client.AccessManagement/Roles/UseCases/MainDataList/RoleMainDataListItem.razor.cs
+++ src/ChronoFlow.Client.AccessManagement/Roles/UseCases/MainDataList/RoleMainDataListItem.razor.cs
@@ -1,8 +1,17 @@
 ﻿using ChronoFlow.Client.AccessManagement.Roles.Entities;
 using ChronoFlow.Client.Common.MainData.UseCases.MainDataList;
+using ChronoFlow.Shared.Common.Objects.Extensions;
 
 namespace ChronoFlow.Client.AccessManagement.Roles.UseCases.MainDataList;
 
 public partial class RoleMainDataListItem : MainDataListItemComponentBase<RoleViewModel>
 {
+    private List<string> GetBottomDetails()
+    {
+        var description = Item.Description.Shorten(50);
+        if (Item.Description?.Length > 50)
+            description += "...";
+
+        return [description];
+    }
 }
