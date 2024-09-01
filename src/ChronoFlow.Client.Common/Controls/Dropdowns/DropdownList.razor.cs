54e92f166df9a217f07dc7cce687587893711720
Nicolai Wolf <119791859+Nico1395@users.noreply.github.com>
2024-09-01 16:41:00 +0200

    Dropdown list component #9

---
+++ src/ChronoFlow.Client.Common/Controls/Dropdowns/DropdownList.razor.cs
@@ -0,0 +1,150 @@
+﻿using ChronoFlow.Client.Common.Controls.Forms;
+using ChronoFlow.Client.Common.Localization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace ChronoFlow.Client.Common.Controls.Dropdowns;
+
+public partial class DropdownList<TItem> : FormControlComponentBase, IAsyncDisposable
+{
+    private IJSObjectReference? _floatingContainerReference;
+    private DotNetObjectReference<DropdownList<TItem>>? _dropdownListReference;
+    private string? _floatingContainerId;
+
+    [Inject]
+    private ILocalizer Localizer { get; set; } = null!;
+
+    [Inject]
+    private IJSRuntime JsRuntime { get; set; } = null!;
+
+    [Parameter]
+    public List<TItem> Items { get; set; } = [];
+
+    [Parameter]
+    public TItem? SelectedItem { get; set; }
+
+    [Parameter]
+    public EventCallback<TItem?> SelectedItemChanged { get; set; }
+
+    [Parameter]
+    public bool Opened { get; set; }
+
+    [Parameter]
+    public EventCallback<bool> OpenedChanged { get; set; }
+
+    [Parameter]
+    public string? Tooltip { get; set; }
+
+    [Parameter]
+    public string? Placeholder { get; set; }
+
+    [Parameter]
+    public bool OnlyShowPlaceholder { get; set; }
+
+    [Parameter]
+    public string? IconLeft { get; set; }
+
+    [Parameter]
+    public string? IconLeftSize { get; set; }
+
+    [Parameter]
+    public string? IconRight { get; set; }
+
+    [Parameter]
+    public string? IconRightSize { get; set; }
+
+    [Parameter]
+    public Func<TItem, string>? DisplayValue { get; set; }
+
+    [Parameter]
+    public Func<TItem, string>? ItemIconLeft { get; set; }
+
+    [Parameter]
+    public Func<TItem, string>? ItemIconRight { get; set; }
+
+    [Parameter]
+    public bool Localize { get; set; }
+
+    [JSInvokable(nameof(OnFocusLostAsync))]
+    public Task OnFocusLostAsync()
+    {
+        return ToggleAsync();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_floatingContainerReference != null)
+            await _floatingContainerReference.InvokeVoidAsync("removeClickOfFocusEventListener");
+
+        _dropdownListReference?.Dispose();
+    }
+
+    protected override void OnParametersSet()
+    {
+        Id ??= Guid.NewGuid().ToString();
+        _floatingContainerId = $"{Id}-items";
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            _floatingContainerReference = await JsRuntime.InvokeAsync<IJSObjectReference>(
+                "createFloatingContainer",
+                _dropdownListReference = DotNetObjectReference.Create(this),
+                _floatingContainerId);
+        }
+    }
+
+    private string GetClasses()
+    {
+        var invalid = !IsValid ? "invalid" : null;
+        var opened = Opened ? "opened" : null;
+        var disabled = GetDisabled() ? "disabled" : null;
+
+        return $"c-dropdown-list {invalid} {opened} {disabled}".Trim();
+    }
+
+    private string GetStyles()
+    {
+        var width = Width != null ? $"width:{Width};" : null;
+        return $"{width}";
+    }
+
+    private string? GetDisplayValue(TItem item)
+    {
+        var displayValue = DisplayValue != null ? DisplayValue(item) : item?.ToString();
+        if (displayValue == null)
+            return null;
+
+        if (Localize)
+            displayValue = Localizer[displayValue];
+
+        return displayValue;
+    }
+
+    private async Task ToggleAsync()
+    {
+        await OpenedChanged.InvokeAsync(Opened = !Opened);
+        await Task.Run(StateHasChanged);
+
+        if (_floatingContainerReference != null)
+        {
+            if (Opened)
+                await _floatingContainerReference.InvokeVoidAsync("onOpen");
+            else
+                await _floatingContainerReference.InvokeVoidAsync("onClose");
+        }
+    }
+
+    private async Task OnSelectItemAsync(TItem item)
+    {
+        if (SelectedItem != null && SelectedItem.Equals(item))
+            SelectedItem = default;
+        else
+            SelectedItem = item;
+
+        await SelectedItemChanged.InvokeAsync(SelectedItem);
+        await ToggleAsync();
+    }
+}
--- src/ChronoFlow.Client/wwwroot/js/app.js
+++ src/ChronoFlow.Client/wwwroot/js/app.js
@@ -1,3 +1,4 @@
+// Object literals
 window.localStorageManager = {
     getItems: function () {
         return localStorage;
@@ -9,3 +10,49 @@ window.sessionStorageManager = {
         return sessionStorage;
     }
 }
+
+// Objects
+class FloatingContainer {
+    componentReference;
+    floatingContainerId;
+    clickOrFocusEventHandler;
+
+    constructor(componentReference, floatingContainerId) {
+        this.componentReference = componentReference;
+        this.floatingContainerId = floatingContainerId;
+        this.clickOrFocusEventHandler = (event) => this.onClickOrFocus(event);
+    }
+
+    onOpen() {
+        this.addClickOfFocusEventListener();
+    }
+
+    onClose() {
+        this.removeClickOfFocusEventListener();
+    }
+
+    addClickOfFocusEventListener() {
+        document.body.addEventListener("click", this.clickOrFocusEventHandler);
+        document.body.addEventListener("focus", this.clickOrFocusEventHandler);
+    }
+
+    removeClickOfFocusEventListener() {
+        document.body.removeEventListener("click", this.clickOrFocusEventHandler);
+        document.body.removeEventListener("focus", this.clickOrFocusEventHandler);
+    }
+
+    onClickOrFocus(event) {
+        const floatingContainer = document.getElementById(this.floatingContainerId);
+        if (floatingContainer === undefined || floatingContainer == null)
+            return;
+
+        if (!floatingContainer.contains(event.target))
+            this.componentReference.invokeMethodAsync("OnFocusLostAsync");
+    }
+}
+
+// Object factory functions
+function createFloatingContainer(componentReference, floatingContainerId) {
+    return new FloatingContainer(componentReference, floatingContainerId);
+}
+
