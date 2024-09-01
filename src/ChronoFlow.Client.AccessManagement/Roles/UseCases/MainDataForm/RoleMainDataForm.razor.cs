4c8e6d0a13795b58940982bc06352067511356e5
Nicolai Wolf <119791859+Nico1395@users.noreply.github.com>
2024-09-01 16:44:05 +0200

    Use the textarea and sort options for permissions in the role form #18

--- src/ChronoFlow.Client.AccessManagement/Roles/UseCases/MainDataForm/RoleMainDataForm.razor.cs
+++ src/ChronoFlow.Client.AccessManagement/Roles/UseCases/MainDataForm/RoleMainDataForm.razor.cs
@@ -1,4 +1,6 @@
-﻿using ChronoFlow.Client.AccessManagement.Roles.Entities;
+﻿using ChronoFlow.Client.AccessManagement.Permissions.Entities;
+using ChronoFlow.Client.AccessManagement.Roles.Entities;
+using ChronoFlow.Client.Common.Controls.Data;
 using ChronoFlow.Client.Common.Localization;
 using ChronoFlow.Client.Common.MainData.UseCases.MainDataForm;
 using Microsoft.AspNetCore.Components;
@@ -22,5 +24,13 @@ public partial class RoleMainDataForm : ComponentBase
             return Localizer["NewRole"];
 
         return Localizer["EditingEntry", context.Item.Name];
+    }
+
+    private List<ListSortOption<PermissionViewModel>> GetPermissionListSortOptions()
+    {
+        var nameAscending = new ListSortOption<PermissionViewModel>(Localizer["NameAscending"], p => p.Name, ListSortDirection.Ascending);
+        var nameDescending = new ListSortOption<PermissionViewModel>(Localizer["NameDescending"], p => p.Name, ListSortDirection.Descending);
+
+        return [nameAscending, nameDescending];
     }
 }
