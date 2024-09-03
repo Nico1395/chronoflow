fa23469a1841c2b77d390f52e153bc25ed81d506
Nicolai Wolf <119791859+Nico1395@users.noreply.github.com>
2024-09-03 20:31:02 +0200

    Form section component #18 #9

---
+++ src/ChronoFlow.Client.Common/Controls/Forms/FormSection.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ChronoFlow.Client.Common.Controls.Forms;
+
+public partial class FormSection : ComponentBase
+{
+    [Parameter, EditorRequired]
+    public required string Title { get; set; }
+
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+}
