dd9bcba0d609c3a5b7c64f27a69480bcbbdb820a
Nicolai Wolf <119791859+Nico1395@users.noreply.github.com>
2024-09-05 19:18:37 +0200

    Cloning adaptation

---
+++ src/ChronoFlow.Shared.Common/Cloning/CloningExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Force.DeepCloner;
+
+namespace ChronoFlow.Shared.Common.Cloning;
+
+public static class CloningExtensions
+{
+    public static T CloneInstance<T>(this T @object)
+    {
+        return @object.DeepClone();
+    }
+}
